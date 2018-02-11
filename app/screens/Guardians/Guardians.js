f3189efa2de1d2e099eaa21c34facbb4d45338dd
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-10 22:22:37 -0600

    Add status bar padding on screen
    So that the content of the screen isnt right underneath the status bar information.

--- app/screens/Guardians/Guardians.js
+++ app/screens/Guardians/Guardians.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import styling from 'app/config/styling'
 
 export default class Guardians extends Component {
 
@@ -9,7 +10,7 @@ export default class Guardians extends Component {
 	 */
 	render() {
 		return (
-			<View>
+			<View style={styling.statusBarPadding}>
 				<Text>This is the guardians screen.</Text>
 			</View>
 		)
