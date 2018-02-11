bfdf89112c8afb94871bcd75472b2c9f385453dc
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-11 01:34:16 -0600

    Add floatingButton sizes and padding to global styling
    Centralizing this information makes it easier to tweak and edit the app's design.

--- app/config/styling.js
+++ app/config/styling.js
@@ -1,14 +1,29 @@
-const styling = {}
+const styling = {};
+const screenPaddingSize = 16;
 
-styling.mainColor = '#8299f3'
-styling.black = '#464d56'
+styling.mainColor = '#8299f3';
+styling.black = '#464d56';
 
 styling.text = {
 	lightgray: '#c0c2c5',
-}
+	font: {
+		light: 'montserrat-regular',
+		heavy: 'montserrat-medium',
+	}
+};
+
+styling.screenPadding = {
+	paddingLeft: screenPaddingSize,
+	paddingRight: screenPaddingSize,
+};
 
 styling.statusBarPadding = {
 	paddingTop: 25,
-}
+};
 
-export default styling
+styling.floatingButton = {
+	size: 35,
+	iconSize: 16,
+};
+
+export default styling;
