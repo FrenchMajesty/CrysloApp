8c26a399f63aa8370b59d1bcd98f6fc0c0c87b75
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-24 00:52:48 -0600

    Add screenSize and text.prop
    I added the screen size so I can use it in computations and I created the text prop so I can easily set the familyFont on Text components.

--- app/config/styling.js
+++ app/config/styling.js
@@ -1,14 +1,32 @@
+import Dimensions from 'Dimensions';
+
+const {height: screenHeight, width: screenWidth} = Dimensions.get('window');
+
 const styling = {};
 const screenPaddingSize = 16;
 
 styling.mainColor = '#8299f3';
 styling.black = '#464d56';
 
+styling.screenSize = {
+	width: screenWidth,
+	height: screenHeight,
+};
+
 styling.text = {
 	lightgray: '#c0c2c5',
 	font: {
 		light: 'montserrat-regular',
 		heavy: 'montserrat-medium',
+	},
+};
+
+styling.text.prop = {
+	light: {
+		fontFamily: styling.text.font.light,
+	},
+	heavy: {
+		fontFamily: styling.text.font.heavy,
 	}
 };
 
