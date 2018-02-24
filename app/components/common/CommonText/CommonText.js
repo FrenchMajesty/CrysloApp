bd43f062bcda8cb0ac9130013211cd200f9e3317
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-24 00:51:57 -0600

    Update CommonText Component to support more customization
    I realized it was annoying to set the color of the text on each and every element if my text had to be white on a certain screen so I created the color prop to simplify that.

--- app/components/common/CommonText/CommonText.js
+++ app/components/common/CommonText/CommonText.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Text } from 'react-native';
 import styling from 'app/config/styling';
 
-export default CommonText = ({weight, style, children}) => {
+export default CommonText = ({weight, color, style, children}) => {
 
 	const fontWeight = weight == 'heavy' ? 'heavy' : 'light';
+	const textColor = {
+		color: color ? color : 'black',
+	};
+
+	const styleProps = [
+		styling.text.prop[fontWeight],
+		textColor,
+		style,
+	];
 
 	return (
-		<Text style={[styling.text.prop[fontWeight], style]}>
+		<Text style={styleProps}>
 			{children}
 		</Text>
 	);
