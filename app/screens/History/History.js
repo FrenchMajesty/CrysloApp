310459960cf43a760a35caa0dc9df4eaea080d26
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-24 02:27:26 -0600

    Update History screen design
    As well as update the placeholder content to make it closer to the real thing.

--- app/screens/History/History.js
+++ app/screens/History/History.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import moment from 'moment';
+import Header from 'app/components/Header/';
+import CommonText from 'app/components/common/CommonText/';
+import DataRow from 'app/components/History/DataRow/DataRow/';
 import styling from 'app/config/styling';
+import style from './style';
 
 export default class History extends Component {
 
@@ -42,19 +46,28 @@ export default class History extends Component {
 	/**
 	 * Renders the statistics of the current day observed
 	 * @param  {Object} day  Date object
-	 * @param  {Object} firstItem First item of that day to display
+	 * @param  {Object} firstMetric First item of that day to display
 	 * @return {ReactElement}      markup
 	 */
-	renderDay(day, firstItem) {
+	renderDay(day, firstMetric) {
 		const {dateExpanded} = this.state;
 
 		if(day) {
+			const readableDate = moment(day.dateString).format('ddd DD');
+
 			return (
-				<View style={{marginTop: 20}}>
-					<View style={{width: 700}}>
-						<Text style={{fontSize: 15}}>{day.dateString}</Text>
-					</View>
-					<Text>{firstItem.text}</Text>
+				<View style={style.dayContainer}>
+					<CommonText style={{
+						flex: 1,
+						alignSelf: 'stretch',
+						backgroundColor: 'yellow',
+					}}>{readableDate}</CommonText>
+					{
+						firstMetric ?
+							<DataRow metric={firstMetric} />
+						:
+							<CommonText>Empty day</CommonText>
+					}
 				</View>
 			);
 		}
@@ -63,55 +76,113 @@ export default class History extends Component {
 	renderDayContent(item, isTheFirstItemOfDay) {
 		const {dateExpanded} = this.state;
 
-		return (
-			<View>
-				<Text>{item.text}</Text>
-			</View>
-		);
+		if(!isTheFirstItemOfDay) {
+			return (<DataRow metric={item} />);
+		}
 	}
-
 
 	/**
 	 * Render the component
 	 * @return {ReactElement} Markup
 	 */
 	render() {
+		const {screenProps: {rootNavigation}} = this.props
 		const {today} = this.state;
 		const minusOne = moment().subtract(1,'d').format('YYYY-MM-DD');
-		const plusOne = moment().add(1,'d').format('YYYY-MM-DD');
-		const plusTwo = moment().add(2,'d').format('YYYY-MM-DD');
+		const minusTwo = moment().subtract(2,'d').format('YYYY-MM-DD');
+		const minusThree = moment().subtract(3,'d').format('YYYY-MM-DD');
 
 		const items = {
 			[minusOne]: [
-				{ text: 'This is meant to represent yesterday 1', date: minusOne},
-				{ text: 'This is meant to represent yesterday 2', date: minusOne},
+				{
+					type: 'heart',
+					name: 'Heart Pulse',
+					value: 88, 
+					date: minusOne,
+				},
+				{
+					type: 'breaths',
+					name: 'Breaths Pulse',
+					value: 17, 
+					date: minusOne,
+				},
 			],
 			[today]: [
-				{ text: 'firs item: Any JS Object here.. but why?', date: today},
-				{ text: 'Any JS Object here.. but why?', date: today},
+				{
+					type: 'sleep',
+					name: 'Sleep',
+					value: '10:20', 
+					date: today,
+				},
+				{
+					type: 'mood',
+					name: 'Mood',
+					value: 'Frustrated', 
+					date: today,
+				},
 			],
-			[plusOne]: [
-				{ text: 'This is the first item of the second day.', date: plusOne },
-				{ text: 'This is the second day.', date: plusOne },
-			],
-			[plusTwo]: [
-				{ text: '3 things in this day just because.', date: plusTwo },
-				{ text: 'Where are you guys?', date: plusTwo },
-				{ text: 'This should also be in the same 3rd day....', date: plusTwo },
+			[minusTwo]: [],
+			[minusThree]: [
+				{
+					type: 'heart',
+					name: 'Heart Pulse',
+					value: 88, 
+					date: minusThree,
+				},
+				{
+					type: 'breaths',
+					name: 'Breaths Pulse',
+					value: 17, 
+					date: minusThree,
+				},
+				{
+					type: 'mood',
+					name: 'Mood',
+					value: 'Happy', 
+					date: minusThree,
+				},
+				{
+					type: 'sleep',
+					name: 'Sleep',
+					value: '7:29', 
+					date: minusThree,
+				},
 			],
 		};
 
+		const white = '#fff';
+		const purple = '#8563e5';
+		const calendarStyle = {
+	   		backgroundColor: white,
+	   		calendarBackground: purple,
+	   		selectedDayBackgroundColor: white,
+	   		selectedDayTextColor: purple,
+	   		dotColor: white,
+	   		dayTextColor: white,
+	   		textDisabledColor: '#ffffff40',
+	   		todayTextColor: '#000',
+	   		monthTextColor: white,
+	   		textSectionTitleColor: '#ffffff80'
+	   	};
+
 		return (
-			<View style={[styling.statusBarPadding, {flex: 1}]}>
+			<View style={[styling.statusBarPadding, {flex: 1, backgroundColor: purple}]}>
+				<StatusBar barStyle="light-content" />
+				<Header 
+					title="History"
+					color="white"
+					style={{flex: 0, backgroundColor: purple}}
+					navigation={rootNavigation}
+				/>
 				<Agenda
 					items={items}
 					loadItemsForMonth={(month) => {console.log('trigger items loading')}}
 				  	onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
 				  	onDayPress={this.updateDateExpanded}
 				  	onDayChange={(day) => console.log('Scrolled onto another day')}
-					//selected={today}
+					selected={today}
 					minDate={moment().subtract(7,'d').format('YYYY-MM-DD')}
-					maxDate={moment().add(7,'d').format('YYYY-MM-DD')}
+					maxDate={moment().add(0,'d').format('YYYY-MM-DD')}
 					pastScrollRange={3}
 					futureScrollRange={1}
 					hideKnob={false}
@@ -119,10 +190,12 @@ export default class History extends Component {
 				  // specify how each date should be rendered. day can be undefined if the item is not first in that day.
 				  	renderDay={this.renderDay}
 				  // specify how empty date content with no items should be rendered
-				  renderEmptyDate={() => {return (<View />);}}
+				  	renderEmptyDate={() => {return (<View />);}}
 				  // specify what should be rendered instead of ActivityIndicator
-				  renderEmptyData = {() => {return (<View />);}}
-				   rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+				  	renderEmptyData = {() => {return (<View />);}}
+				   	rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+				   	style={{flex: 1}}
+				   	theme={calendarStyle}
 				/>
 			</View>
 		);
---
+++ app/screens/History/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	dayContainer: {
+		marginTop: 20,
+		display: 'flex',
+		flexDirection: 'column',
+		width: '100%',
+	},
+});
