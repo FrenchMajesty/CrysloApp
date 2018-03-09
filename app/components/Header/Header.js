7d508726ca17feef084e8de7a80c43d7976ed168
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-09 10:09:58 -0600

    Replace MenuButton component with IconButton
    I'm removing MenuButton because IconButton is more versatile. I can't create a component for every button action I want to have it's not substainable so I created the IconButton component to make it easier for me to create multi-purpose buttons.

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import MenuButton from '../MenuButton/';
+import IconButton from '../common/Button/IconButton/';
 import style from './style';
 import styling from 'app/config/styling';
 
 const Header = ({title, navigation, style: customStyle, color}) => {
 
+	const openMenu = () => {
+		navigation.navigate('DrawerToggle');
+	};
+
 	return (
 		<View style={[styling.screenPadding, style.container, customStyle]}>
-			<MenuButton navigation={navigation} style={{position: 'absolute', marginLeft: 10}} />
+			<IconButton
+				icon="bars"
+				style={{position: 'absolute', marginLeft: 10}}
+				onPress={openMenu}
+			/>
 			<Text 
 				style={[style.title, {color: color}]} 
 				OnPress={() => navigation.navigate('DrawerToggle')}
