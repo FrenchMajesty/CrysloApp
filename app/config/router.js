909b51c5f105f5e1bf21bc5663c3df6fd2f33150
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-09 01:20:17 -0600

    Add DetailedHistory to router
    So that the screen ca be navigated to from History alone (for now).

--- app/config/router.js
+++ app/config/router.js
@@ -6,11 +6,31 @@ import Dashboard from 'app/screens/Dashboard/';
 import History from 'app/screens/History/';
 import Guardians from 'app/screens/Guardians/';
 import WeCare from 'app/screens/WeCare/';
+import DetailedHistory from 'app/screens/DetailedHistory/';
 
 // Drawers screens
 import Syncing from 'app/screens/Syncing/';
 
 import styling from 'app/config/styling';
+
+const HistoryTab = StackNavigator({
+	History: {
+		screen: History,
+		path: '/',
+		navigationOptions: {
+			header: null,
+		},
+	},
+	Details: {
+		path: '/history/:vital',
+		screen: DetailedHistory,
+		navigationOptions: ({navigation}) => ({
+			vital: navigation.state.params.vital,
+			header: null,
+		})
+	}
+});
+
 
 const SimpleTabs = TabNavigator(
 	{
@@ -23,7 +43,7 @@ const SimpleTabs = TabNavigator(
 			}
 		},
 		History: {
-			screen: History,
+			screen: HistoryTab,
 			navigationOptions: {
 				tabBarIcon: ({focused, tintColor}) => {
 					return <Icon name="bar-chart" size={25} color={tintColor} />
