d6ac929ba6bc10ab2f9992548793b7f7ccfb654d
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-09 21:33:50 -0600

    Create Guardians view

--- app/screens/Guardians/Guardians.js
+++ app/screens/Guardians/Guardians.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Switch, Text } from 'react-native';
 import Header from 'app/components/Header/';
+import Slider from 'app/components/common/Slider/';
+import RoundedButton from 'app/components/common/Button/RoundedButton/';
 import styling from 'app/config/styling';
+import style from './style';
 
 export default class Guardians extends Component {
 
@@ -10,6 +13,104 @@ export default class Guardians extends Component {
 	 */
 	constructor(props) {
 		super(props);
+
+		this.state = this.getInitialState();
+
+		this.renderAlerts = this.renderAlerts.bind(this);
+		this.renderBloodPressure = this.renderBloodPressure.bind(this);
+	}
+
+	/**
+	 * Get the initial state of the component
+	 * @return {Object} 
+	 */
+	getInitialState() {
+		return {
+			notifyMe: false,
+			notifyWeCare: true,
+			notifySOS: true,
+			bloodMin: 85,
+			bloodMax: 105,
+		};
+	}
+
+	/**
+	 * Render the alert section of the form
+	 * @return {ReactElement}
+	 */
+	renderAlerts() {
+		const {notifyMe, notifySOS, notifyWeCare} = this.state;
+		return (
+			<View>
+				<Text style={[styling.text.prop.heavy, style.sectionTitle]}>Alerts</Text>
+				<View style={[style.alertOption]}>
+					<Switch
+						onTintColor={styling.mainColor}
+						onValueChange={() => this.setState({notifyMe: !notifyMe})}
+						value={notifyMe}
+					/>
+					<Text style={[styling.text.prop.light, style.alertText]}>Notify me</Text>
+				</View>
+				<View style={[style.alertOption]}>
+					<Switch
+						onTintColor={styling.mainColor}
+						onValueChange={() => this.setState({notifyWeCare: !notifyWeCare})}
+						value={notifyWeCare}
+					/>
+					<Text style={[styling.text.prop.light, style.alertText]}>Notify my WeCare followers</Text>
+				</View>
+				<View style={[style.alertOption]}>
+					<Switch
+						onTintColor={styling.mainColor}
+						onValueChange={() => this.setState({notifySOS: !notifySOS})}
+						value={notifySOS}
+					/>
+					<Text style={[styling.text.prop.light, style.alertText]}>Notify my emergency contacts</Text>
+				</View>
+			</View>
+		);
+	}
+
+	/**
+	 * Render the blood pressure section of the form
+	 * @return {ReactElement} 
+	 */
+	renderBloodPressure() {
+		const {bloodMin, bloodMax} = this.state;
+
+		return (
+			<View>
+				<Text style={[styling.text.prop.heavy, style.sectionTitle]}>Blood Pressure</Text>
+				
+				<Text style={[styling.text.prop.light, style.sectionSubtitle]}>Systollic</Text>
+				<View style={[style.sliderContainer]}>	
+					<View style={[style.sliderWrapper]}>
+						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{bloodMin}</Text>
+						<Slider 
+							label="Minimum"
+							minimumValue={80}
+							maximumValue={120}
+							step={5}
+							value={bloodMin}
+							onValueChange={(newVal) => this.setState({bloodMin: newVal})}
+							style={{flex: 1}}
+						/>
+					</View>
+					<View style={[style.sliderWrapper]}>
+						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{bloodMax}</Text>
+						<Slider 
+							label="Maximum"
+							minimumValue={100}
+							maximumValue={160}
+							step={5}
+							value={bloodMax}
+							onValueChange={(newVal) => this.setState({bloodMax: newVal})}
+							style={{flex: 1}}
+						/>
+					</View>
+				</View>
+			</View>
+		);
 	}
 
 	/**
@@ -18,20 +119,30 @@ export default class Guardians extends Component {
 	 */
 	render() {
 		const {rootNavigation} = this.props.screenProps;
-
 		return (
-			<ScrollView style={[styling.container]}>
-				<Header
-					navigation={rootNavigation}
-					title="Guardians"
-					hint="This is where you can update your parameters for your Helo watch."
-					elevated={true}
-					style={{flex: 2}}
-				/>
-				<View style={[styling.screenPadding]}>
-					<Text>This is the guardians screen.</Text>
+			<View style={[styling.container, styling.grayScreenBackground]}>
+				<View style={{backgroundColor: '#fff', height: 140}}>
+					<Header
+						navigation={rootNavigation}
+						title="Guardians"
+						color={styling.black}
+						hint="This is where you can update your parameters for your Helo watch."
+						elevated={true}
+						style={{flex: 2, position: 'absolute'}}
+					/>
 				</View>
-			</ScrollView>
+				<ScrollView style={[styling.screenPadding]}>
+					{this.renderAlerts()}
+					{this.renderBloodPressure()}
+
+					<View style={[style.buttonContainer]}>
+						<RoundedButton
+							text="Save Changes"
+							center={true}
+						/>
+					</View>
+				</ScrollView>
+			</View>
 		);
 	}
 };
---
+++ app/screens/Guardians/style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+import styling from 'app/config/styling';
+
+export default StyleSheet.create({
+	sectionTitle: {
+		fontFamily: styling.text.font.heavy,
+		color: styling.black,
+		fontSize: 23,
+		marginTop: 25,
+		marginBottom: 25,
+	},
+	sectionSubtitle: {
+		fontSize: 18,
+		color: styling.mainColor,
+		marginBottom: 20,
+	},
+	alertOption: {
+		display: 'flex',
+		flexDirection: 'row',
+		marginTop: 8,
+		marginBottom: 8,
+	},
+	alertText: {
+		color: styling.text.gray,
+		marginLeft: 20,
+		fontSize: 17,
+		lineHeight: 28,
+	},
+	sliderValue: {
+		color: styling.text.darkgray,
+		textAlign: 'center',
+		marginRight: 15,
+	},
+	sliderContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	sliderWrapper: {
+		flexDirection: 'column',
+		flex: 1,
+	},
+	buttonContainer: {
+		alignItems: 'center',
+		marginTop: 30,
+		paddingBottom: 170,
+	}
+});
