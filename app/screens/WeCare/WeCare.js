5154260dc31b1a208e2e69b7930531cafcd33578
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-10 14:42:13 -0600

    Add placeholder content & style the WeCare screen

--- app/screens/WeCare/WeCare.js
+++ app/screens/WeCare/WeCare.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import Header from 'app/components/Header/';
+import Button from 'app/components/common/Button/RectangularButton/';
+import ContactList from 'app/components/WeCare/ContactList/';
+import style from './style';
+import styling from 'app/config/styling';
 
 export default class WeCare extends Component {
 
@@ -7,7 +12,51 @@ export default class WeCare extends Component {
 	 * Component constructor
 	 */
 	constructor(props) {
-		super(props)
+		super(props);
+
+		this.state = this.getInitialState();
+	}
+
+	getInitialState() {
+		return {
+			contacts: [
+				{
+					id: 2,
+					name: 'John Dope',
+					number: '+1 (123)-444-3309',
+				},
+				{
+					id: 3,
+					name: 'Marie Antoinette',
+					number: '+33 7 33 78 56 29',
+				},
+			]
+		};
+	}
+
+	/**
+	 * Render the button to add additional emergency contacts
+	 * @return {ReactElement} 
+	 */
+	renderAddButton() {
+		const {contacts} = this.state;
+
+		if(contacts.length >= 3) {
+			return (
+				<Button
+					text="You have reached the limit"
+					style={{width: '70%'}}
+					disabled={true}
+				/>
+			);
+		}
+
+		return (
+			<Button
+				text={`Add ${3-contacts.length} more`}
+				style={{width: '70%'}}
+			/>
+		);
 	}
 
 	/**
@@ -15,10 +64,30 @@ export default class WeCare extends Component {
 	 * @return {ReactElement} Markup
 	 */
 	render() {
+		const {rootNavigation} = this.props.screenProps;
+		const {contacts} = this.state;
+
 		return (
-			<View>
-				<Text>Hey man! this is the WeCare view screen of the app..</Text>
+			<View style={[styling.container, styling.grayScreenBackground]}>
+				<View style={{backgroundColor: '#fff', height: 140}}>
+					<Header
+						navigation={rootNavigation}
+						title="WeCare"
+						color={styling.black}
+						hint="These contacts are the people we are going to notify via text or call if your vitals go into your danger zone."
+						elevated={true}
+						style={{flex: 2, position: 'absolute'}}
+					/>
+				</View>
+				<ScrollView style={[style.screenPadding, style.listContainer]}>
+					<ContactList 
+						data={contacts} 
+					/>
+					<View style={[style.buttonStyle]}>
+						{this.renderAddButton()}
+					</View>
+				</ScrollView>
 			</View>
-		)
+		);
 	}
-}
+};
---
+++ app/screens/WeCare/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	listContainer: {
+		height: '100%',
+		paddingTop: 10,
+		paddingBottom: 15,
+		marginTop: 23,
+	},
+	buttonStyle: {
+		alignItems: 'center',
+		marginTop: 30,
+		paddingBottom: 70,
+	}
+});
