6dcefd00de20ba5c80ac2aa6b500aba86c828f63
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-24 03:04:33 -0500

    Create Login screen

---
+++ app/screens/Login/Login.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo';
+import RoundedButton from 'app/components/common/Button/RoundedButton';
+import Input from 'app/components/common/Input/TextWithIcon/';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IosIcon from 'react-native-vector-icons/Ionicons';
+import styling from 'app/config/styling'; 
+import style from './style';
+
+
+class Login extends Component {
+
+	/**
+	 * Component constructor
+	 */
+	constructor(props) {
+		super(props);
+
+		this.state = this.getInitialState();
+	}
+
+	/**
+	 * Return the initial state of the screen component
+	 * @return {Object} 
+	 */
+	getInitialState() {
+		return {
+			email: '',
+			password: '',
+			passwordIsHidden: true,
+		};
+	}
+
+	/**
+	 * Render the component
+	 * @return {ReactElement} 
+	 */
+	render() {
+		const {email, password, passwordIsHidden} = this.state;
+
+		return (
+			<View>
+				<LinearGradient 
+					start={[0.1,0.1]}
+					colors={styling.gradient} 
+					style={[styling.screenPadding, styling.statusBarPadding, style.gradientBg, style.container]}
+				>
+					<Text>[App Logo Placeholder]</Text>
+					<View style={{flex: 2}} />
+					<View style={[style.formContainer]}>
+						<View style={[style.inputContainer]}>
+							<Input
+								iconColor="white"
+								backgroundColor="#ffffff12"
+								placeholderTextColor="white"
+								selectionColor="#fff"
+								iconName="envelope"
+								placeholder="Enter your email"
+								keyboardType="email-address"
+								returnKeyType="done"
+								value={email}
+								onChangeText={(email) => this.setState({email})}
+							/>
+						</View>
+						<View style={[style.inputContainer]}>
+							<Input
+								iconColor="white"
+								backgroundColor="#ffffff12"
+								placeholderTextColor="white"
+								selectionColor="#fff"
+								iconName="lock"
+								secureTextEntry={passwordIsHidden}
+								placeholder="Enter your password"
+								returnKeyType="done"
+								value={password}
+								onChangeText={(password) => this.setState({password})}
+							/>
+						</View>
+						<View style={[style.submitContainer]}>
+							<RoundedButton
+								style={[style.submitButton]}
+								text="Sign In"
+							/>
+						</View>
+						<View style={[style.touchId]}>
+							<Text style={[style.text, {marginTop: 10, marginRight: 5}]}>
+								Or login with your Touch ID {"\b"}
+								</Text>
+							<TouchableOpacity>
+								<IosIcon name="ios-finger-print" size={40} color="white" />
+							</TouchableOpacity>
+						</View>
+					</View>
+					<View style={{flex: 3, marginTop: 25}}>
+					<TouchableOpacity>
+						<Text style={[style.text]}>
+							Forgot your password?</Text>
+					</TouchableOpacity>
+					</View>
+					<TouchableOpacity>
+						<Text style={[style.text, {marginBottom: 20}]}>
+							Don't have an account? {"\b"}
+							<Text style={[style.underline]}>Sign Up</Text>
+						</Text>
+					</TouchableOpacity>
+				</LinearGradient>
+			</View>
+		);
+	}
+}
+
+export default Login;
---
+++ app/screens/Login/style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+import styling from 'app/config/styling';
+
+export default StyleSheet.create({
+	gradientBg: {
+		height: '100%',
+	},
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		//alignContent: 'center',
+		//alignItems: 'center',
+	},
+	text: {
+		...styling.text.prop.light,
+		color: 'white',
+		textAlign: 'center',
+	},
+	touchId: {
+		flex: 0,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginTop: 10,
+	},
+	underline: {
+		textDecorationLine: 'underline',
+	},
+	formContainer: {
+		flex: 4,
+		alignSelf: 'center',
+		width: 350,
+	},
+	inputContainer: {
+		flex: 1,
+		width: '100%',
+		margin: 10,
+	},
+	submitContainer: {
+		flex: 1,
+		marginTop: 20,
+	},
+	submitButton: {
+		flex: 0,
+		width: '80%',
+		alignSelf: 'center'
+	},
+});
