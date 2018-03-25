35a7be742f302403ec7752cb713c88b23c36270f
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-25 00:22:37 -0500

    Create ForgotPassword screen
    Because it's part of the AuthStack navigation.

---
+++ app/screens/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class ForgotPassword extends Component {
+
+	/**
+	 * Component constructor
+	 */
+	constructor(props) {
+		super(props);
+	}
+
+	/**
+	 * Render the component
+	 * @return {ReactElement} 
+	 */
+	render() {
+		return (
+			<Text>FORGOT PASSWORD</Text>
+		);
+	}
+}
+
+export default ForgotPassword;
