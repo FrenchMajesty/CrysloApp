92718780a0e1bccdc73dc4bd1a3ac263b755ed71
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-29 00:49:36 -0500

    Update VitalCard component to refresh every minute
    So that the the timestamp on the card can be updated automatically without requiring user input.

--- app/screens/Dashboard/components/VitalCard/VitalCard.js
+++ app/screens/Dashboard/components/VitalCard/VitalCard.js
@@ -7,17 +7,82 @@ import style from './style';
 export default class VitalCard extends React.Component {
 
 	/**
+	 * Component constructor
+	 */
+	constructor(props) {
+		super(props);
+
+		this.state = this.getInitialState();
+
+		this.timer = this.timer.bind(this);
+	}
+
+	/**
+	 * Return the initial state of the component
+	 * @return {Object} 
+	 */
+	getInitialState() {
+		return {
+			time: null,
+			intervalId: null,
+		};
+	}
+
+	/**
+	 * Set the time value and create the time updating interval at every second
+	 */
+	componentWillMount() {
+		const {lastReading} = this.props.item;
+
+		if(lastReading) {
+			const time = distanceInWords(lastReading, new Date());
+			const intervalId = setInterval(this.timer, 60000);
+
+			this.setState({time, intervalId});
+		}
+	}
+
+	/**
+	 * Set the time value and create the time updating interval at every second
+	 */
+	componentWillReceiveProps({item: {lastReading}}) {
+		const {intervalId} = this.state;
+
+		if(!intervalId) {
+			const time = distanceInWords(lastReading, new Date());
+			const newIntervalId = setInterval(this.timer, 60000);
+			clearInterval(intervalId);
+
+			this.setState({time, intervalId: newIntervalId});
+		}
+	}
+
+	/**
+	 * Clear the interval timer created at the mounting of the component
+	 */
+	componentWillUnmount() {
+		clearInterval(this.state.intervalId);
+	}
+
+	timer() {
+		const {lastReading} = this.props.item;
+		this.setState({time: distanceInWords(lastReading, new Date())});
+	}
+
+	/**
 	 * Render the component
 	 * @return {ReactElement} 
 	 */
 	render() {
-		const {children, item, onPress} = this.props
+		const {children, item, onPress} = this.props;
+		const {time} = this.state;
+
 		return (
 			<Card card={style.card} body={style.body} onPress={onPress}>
 				<Text style={style.name}>{item.name}</Text>
 				<View style={style.logo} />
-				<Text style={style.vitalValue}>{item.value} Peanuts</Text>
-				<Text style={style.date}>{distanceInWords(item.lastReading, new Date())} ago</Text>
+				<Text style={style.vitalValue}>{item.value} {item.measure}</Text>
+				<Text style={style.date}>{time} ago</Text>
 			</Card>
 		);
 	}
