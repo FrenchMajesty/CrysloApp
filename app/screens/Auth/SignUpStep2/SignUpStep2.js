aabe5f644144dbff0b5cb8df1312ae0d480f945b
Verdi.K <verdi@victron-tech.com>
2018-04-22 01:18:24 -0500

    Update SignUpStep2 screen to add number verifcation containers
    I recently created the SignUpVerification and SignUpVerificationCode containers so I can use them in this screen to verify a users' phone number via sending a text.

--- app/screens/Auth/SignUpStep2/SignUpStep2.js
+++ app/screens/Auth/SignUpStep2/SignUpStep2.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
+import { connect } from 'react-redux';
 import IconButton from 'app/components/common/Button/IconButton/';
 import Input from 'app/components/common/Input/TextWithIcon/';
+import PhoneVerification from 'app/container/SignUpVerification/';
+import ValidateSecurityCode from 'app/container/SignUpVerificationCode/';
 import styling from 'app/config/styling';
-import style from './style';
+import style from '../style';
 import phoneImg from '../../../../assets/images/text.png';
 
 class SignUpStep2 extends Component {
@@ -13,6 +16,24 @@ class SignUpStep2 extends Component {
 	 */
 	constructor(props) {
 		super(props);
+
+		this.state = this.getInitialState();
+
+		this.numberSubmitted = this.numberSubmitted.bind(this);
+	}
+
+	/**
+	 * Return the initial state of the component
+	 * @return {Object} 
+	 */
+	getInitialState() {
+		return {
+			number: '',
+		}
+	}
+
+	numberSubmitted(number) {
+		this.setState({number});
 	}
 
 	/**
@@ -20,24 +41,34 @@ class SignUpStep2 extends Component {
 	 * @return {ReactElement} 
 	 */
 	render() {
-		const {navigation} = this.props;
+		const {navigation, signup: {number}} = this.props;
 
 		return (
-			<View style={[styling.statusBarPadding, style.container]}>
+			<View style={[styling.statusBarPadding, style.container, {flex: 1}]}>
 				<IconButton
 					icon="arrow-left"
 					style={styling.fixedNavButton}
 					onPress={() => navigation.goBack(null)}
 				/>
-				<View style={{flex: 1}}>
-					<Image source={phoneImg} />
-				</View>
-				<View style={{flex: 2}}>
-					<Text>Enter your number here..</Text>
+				<View style={[{alignSelf: 'center', top: '10%'}]}>
+					{number ?
+						<ValidateSecurityCode />
+					: 
+						<PhoneVerification />
+					}
 				</View>
 			</View>
 		);
 	}
 }
 
-export default SignUpStep2;
+/**
+ * Map the redux store's state to the component's props
+ * @param  {Object} store.signup User's sign up data
+ * @return {Object}                  
+ */
+const mapStateToProps = ({signup}) => ({
+		signup,
+})
+
+export default connect(mapStateToProps)(SignUpStep2);
