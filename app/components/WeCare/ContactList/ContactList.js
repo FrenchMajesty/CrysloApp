f5ec6143e17beaae742ef28a0ead8e39bb15a6bd
Verdi.K <verdi@victron-tech.com>
2018-04-26 22:02:12 -0500

    Replace ContactRow with ListItem
    I modified the ContactRow component to ListItem becuase I needed to build a list for the Settings screen and the ContactRow component was perfect for the task so instead of adding more stuffs I just tweaked it to be broader.

--- app/components/WeCare/ContactList/ContactList.js
+++ app/components/WeCare/ContactList/ContactList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import ContactRow from '../ContactRow/';
+import ListItem from 'app/components/common/ListItem/';
 import style from './style';
 
 const ContactList = ({data, onItemPress}) => {
 
 	const separator = () => <View style={[style.separator]} />;
 
-	const row = ({item}) => <ContactRow item={item} onPress={onItemPress} />;
+	const row = ({item}) => {
+		return (<ListItem 
+					primaryText={item.name} 
+					secondaryText={item.number}
+					onPress={onItemPress} 
+				/>);
+	};
+
 	return (
 		<FlatList
 			ItemSeparatorComponent={separator}
