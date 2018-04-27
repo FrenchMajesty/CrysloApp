4a9890217f229ea32c0870a18fe068fab4dd2cab
Verdi.K <verdi@victron-tech.com>
2018-04-26 22:00:31 -0500

    Update ContactRow component to ListItem
    I renamed it and updated the composition of the component so that it can be broad enough for me to use it in different lists other than the contact list.

---
+++ app/components/common/ListItem/ListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import style from './style';
+
+const ListItem = ({primaryText, secondaryText, onPress}) => {
+
+	return (
+		<View>
+			<TouchableOpacity 
+				activeOpacity={0.6}
+				style={[style.row]}
+				onPress={onPress}
+			>
+				<View style={[style.rowContent]}>
+					<Text style={[style.name]}>{primaryText}</Text>
+					<Text style={[style.number]}>{secondaryText}</Text>
+				</View>
+				<Icon name="ios-arrow-forward" size={24} style={style.black} />
+			</TouchableOpacity>
+			<View style={[style.separator]} />
+		</View>
+	);
+};
+
+export default ListItem;
