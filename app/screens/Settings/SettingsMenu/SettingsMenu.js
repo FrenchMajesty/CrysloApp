d622ae42b8ffcc187481ef4aa005bdbf43b72e6c
Verdi.K <verdi@victron-tech.com>
2018-04-27 13:37:14 -0500

    Create a folder for the Settings screens
    I realized that in the settings screen, there would be many sub-screens and that I needed to have a navigation stack for it so I created the /Settings  folder and renamed the Settings screen to a more fitting name, SettingsMenu.

---
+++ app/screens/Settings/SettingsMenu/SettingsMenu.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import Header from 'app/components/Header/';
+import Button from 'app/components/common/Button/RectangularButton/';
+import ListItem from 'app/components/common/ListItem';
+import styling from 'app/config/styling';
+import style from './style';
+
+const SettingsMenu = ({navigation}) => {
+
+	return (
+		<View style={[styling.container, styling.grayScreenBackground]}>
+			<View style={{backgroundColor: '#fff', height: 120}}>
+				<Header
+					navigation={navigation}
+					title="Settings"
+					color={styling.black}
+					hint="Here is the place where you update all of your account and app settings."
+					elevated={true}
+					style={{flex: 2, position: 'absolute'}}
+				/>
+			</View>
+			<ScrollView style={[style.screenPadding, style.listContainer]}>
+				<ListItem 
+					primaryText="Account settings" 
+					secondaryText="Here you can check all that stuff out bro."
+					onPress={() => navigation.navigate('AccountSettings')}
+				/>
+				<ListItem 
+					primaryText="Terms and Conditions of Use" 
+					secondaryText="In case you want to review our terms."
+					onPress={() => { /* navigate to the right screen here */ }}
+				/>
+				<ListItem 
+					primaryText="Privacy Policy" 
+					secondaryText="We respect your privacy and data, read our policy."
+				onPress={() => { /* navigate to the right screen here */ }}
+				/>
+			</ScrollView>
+		</View>
+	);
+};
+
+export default SettingsMenu;
