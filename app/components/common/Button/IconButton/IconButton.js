9c88b6d3a0126d9aa93a509049f893e16987ecf0
Verdi.K <verdi@victron-tech.com>
2018-05-05 18:34:58 -0500

    Add inverted and color prop on IconButton component
    I was messing around with it for the ContactDetails screen and I expanded the API so I could try out different colors.

--- app/components/common/Button/IconButton/IconButton.js
+++ app/components/common/Button/IconButton/IconButton.js
@@ -3,11 +3,12 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import FloatingIcon from '../FloatingIcon/';
 import styling from 'app/config/styling';
 
-const IconButton = ({icon, onPress, style: customStyle}) => {
+const IconButton = ({icon, onPress, inverted, style: customStyle, color}) => {
+
 
 	return (
-		<FloatingIcon onPress={onPress} customStyle={customStyle}>
-    		<Icon name={icon} size={styling.floatingButton.iconSize} />
+		<FloatingIcon onPress={onPress} customStyle={[customStyle, inverted ? {backgroundColor: color} : null]}>
+    		<Icon name={icon} color={inverted ? '#fff' : color} size={styling.floatingButton.iconSize} />
     	</FloatingIcon>
     );
     	
