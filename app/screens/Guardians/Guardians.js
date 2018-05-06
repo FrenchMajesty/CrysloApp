ec5d94c4954b70c1dba2cd851b6d926c56b16ac9
Verdi.K <verdi@victron-tech.com>
2018-05-05 22:54:39 -0500

    Update Guardians screen and connect it to the store
    While I was at it I replaced the 'blood pressure' concept with the heart beat concept because that is something that is going to be easy to test when I can finally connect the watch.

--- app/screens/Guardians/Guardians.js
+++ app/screens/Guardians/Guardians.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Switch, Text } from 'react-native';
+import { ScrollView, View, Switch, Text, ActivityIndicator as Spinner } from 'react-native';
+import { connect } from 'react-redux';
 import Header from 'app/components/Header/';
 import Slider from 'app/components/common/Slider/';
 import RoundedButton from 'app/components/common/Button/RoundedButton/';
+import ProfileAction from 'app/store/actions/profile';
 import styling from 'app/config/styling';
 import style from './style';
 
-export default class Guardians extends Component {
+class Guardians extends Component {
 
 	/**
 	 * Component constructor
@@ -16,8 +18,9 @@ export default class Guardians extends Component {
 
 		this.state = this.getInitialState();
 
+		this.onSave = this.onSave.bind(this);
 		this.renderAlerts = this.renderAlerts.bind(this);
-		this.renderBloodPressure = this.renderBloodPressure.bind(this);
+		this.renderHeartSettings = this.renderHeartSettings.bind(this);
 	}
 
 	/**
@@ -26,11 +29,49 @@ export default class Guardians extends Component {
 	 */
 	getInitialState() {
 		return {
-			notifyMe: false,
-			notifySOS: true,
-			bloodMin: 85,
-			bloodMax: 105,
+			notifyMe: null,
+			notifyWeCare: null,
+			heartMin: null,
+			heartMax: null,
+			isSubmitting: false,
 		};
+	}
+
+	/**
+	 * Transfer the guardians data from props to the component's state for modification
+	 */
+	componentWillMount() {
+		const {notify, configs} = this.props.guardians;
+
+		this.setState({
+			notifyMe: notify.self,
+			notifyWeCare: notify.wecare,
+			heartMin: configs.heart.min,
+			heartMax: configs.heart.max,
+		});
+	}
+
+	/**
+	 * When the button is pressed, submit the new configs to the server
+	 * @return {Void} 
+	 */
+	onSave() {
+		this.setState({isSubmitting: true});
+		setTimeout(() => {
+			this.setState({isSubmitting: false});
+			this.props.updateGuardiansConfigs({
+				notify: {
+					self: this.state.notifyMe,
+					wecare: this.state.notifyWeCare,
+				},
+				configs: {
+					heart: {
+						min: this.state.heartMin,
+						max: this.state.heartMax,
+					},
+				}
+			});
+		}, 1000);
 	}
 
 	/**
@@ -38,7 +79,7 @@ export default class Guardians extends Component {
 	 * @return {ReactElement}
 	 */
 	renderAlerts() {
-		const {notifyMe, notifySOS, notifyWeCare} = this.state;
+		const {notifyMe, notifyWeCare} = this.state;
 		return (
 			<View>
 				<Text style={[styling.text.prop.heavy, style.sectionTitle]}>Alerts</Text>
@@ -53,8 +94,8 @@ export default class Guardians extends Component {
 				<View style={[style.alertOption]}>
 					<Switch
 						onTintColor={styling.mainColor}
-						onValueChange={() => this.setState({notifySOS: !notifySOS})}
-						value={notifySOS}
+						onValueChange={() => this.setState({notifyWeCare: !notifyWeCare})}
+						value={notifyWeCare}
 					/>
 					<Text style={[styling.text.prop.light, style.alertText]}>Notify my emergency contacts</Text>
 				</View>
@@ -63,39 +104,39 @@ export default class Guardians extends Component {
 	}
 
 	/**
-	 * Render the blood pressure section of the form
+	 * Render the heartbeat's section of the form
 	 * @return {ReactElement} 
 	 */
-	renderBloodPressure() {
-		const {bloodMin, bloodMax} = this.state;
+	renderHeartSettings() {
+		const {heartMin, heartMax} = this.state;
 
 		return (
 			<View>
-				<Text style={[styling.text.prop.heavy, style.sectionTitle]}>Blood Pressure</Text>
+				<Text style={[styling.text.prop.heavy, style.sectionTitle]}>Heart Beat</Text>
 				
-				<Text style={[styling.text.prop.light, style.sectionSubtitle]}>Systollic</Text>
+				{/*<Text style={[styling.text.prop.light, style.sectionSubtitle]}>Systollic</Text>*/}
 				<View style={[style.sliderContainer]}>	
 					<View style={[style.sliderWrapper]}>
-						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{bloodMin}</Text>
+						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{heartMin}</Text>
 						<Slider 
 							label="Minimum"
-							minimumValue={80}
-							maximumValue={120}
+							minimumValue={30}
+							maximumValue={80}
 							step={5}
-							value={bloodMin}
-							onValueChange={(newVal) => this.setState({bloodMin: newVal})}
+							value={heartMin}
+							onValueChange={(newVal) => this.setState({heartMin: newVal})}
 							style={{flex: 1}}
 						/>
 					</View>
 					<View style={[style.sliderWrapper]}>
-						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{bloodMax}</Text>
+						<Text style={[styling.text.prop.heavy, style.sliderValue]}>{heartMax}</Text>
 						<Slider 
 							label="Maximum"
-							minimumValue={100}
-							maximumValue={160}
+							minimumValue={60}
+							maximumValue={180}
 							step={5}
-							value={bloodMax}
-							onValueChange={(newVal) => this.setState({bloodMax: newVal})}
+							value={heartMax}
+							onValueChange={(newVal) => this.setState({heartMax: newVal})}
 							style={{flex: 1}}
 						/>
 					</View>
@@ -109,32 +150,64 @@ export default class Guardians extends Component {
 	 * @return {ReactElement} 
 	 */
 	render() {
+		const {isSubmitting} = this.state;
 		const {rootNavigation} = this.props.screenProps;
+
 		return (
 			<View style={[styling.container, styling.grayScreenBackground]}>
 				<View style={{backgroundColor: '#fff', height: 140, marginBottom: 23}}>
 					<Header
 						navigation={rootNavigation}
 						title="Guardians"
-						color={styling.black}
 						hint="Guardians helps you set notifications if your health indicators are outside of their normal range."
 						elevated={true}
 						style={{flex: 2, position: 'absolute'}}
 					/>
 				</View>
+
 				<ScrollView style={[styling.screenPadding]}>
 					{this.renderAlerts()}
-					{this.renderBloodPressure()}
+					{this.renderHeartSettings()}
 
 					<View style={[style.buttonContainer]}>
-						<RoundedButton
-							text="Save Changes"
-							inverted={true}
-							center={true}
-						/>
+						{isSubmitting ?
+							<RoundedButton
+								inverted={true}
+								center={true}
+							><Spinner color="#fff" /></RoundedButton>
+						:
+							<RoundedButton
+								text="Save Changes"
+								inverted={true}
+								center={true}
+								onPress={this.onSave}
+							/>	
+						}
 					</View>
 				</ScrollView>
 			</View>
 		);
 	}
 };
+
+/**
+ * Map the redux store's state to the component's props
+ * @param  {Object} options.profile.guardians Tree of the Guardians settings
+ * @return {Object}                  
+ */
+const mapStateToProps = ({profile: {guardians}}) => ({
+		guardians,
+});
+
+/**
+ * Map the store's action dispatcher to the component's props
+ * @param  {Function} dispatch The dispatch function
+ * @return {Object}           
+ */
+const mapDispatchToProps = (dispatch) => ({
+	updateGuardiansConfigs: (newConfigs) => {
+		dispatch(ProfileAction.updateGuardiansConfigs(newConfigs));
+	},
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Guardians);
