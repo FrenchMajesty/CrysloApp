bdabd6adbb62fc3c42b225cc562622b31aa33774
Verdi.K <verdi@victron-tech.com>
2018-05-06 18:13:29 -0500

    Add weCare@updateContact action

--- app/store/actions/wecare.js
+++ app/store/actions/wecare.js
@@ -1,17 +1,35 @@
 export default class WeCare {
 
 	static ADD_NEW_CONTACT = 'ADD_NEW_CONTACT';
+	static UPDATE_CONTACT = 'UPDATE_CONTACT';
 	static DELETE_CONTACT = 'DELETE_CONTACT';
 
 	/**
 	 * Return an action to add a new WeCare contact
-	 * @param {String} options.id   The contact's ID
+	 * @param {Number} options.id   The contact's ID
 	 * @param {String} options.name   The contact's name
 	 * @param {String} options.number The contact's phone number
+	 * @return {Object} 
 	 */
 	static addNewContact({id, name, number}) {
 		return {
 			type: this.ADD_NEW_CONTACT,
+			id,
+			name,
+			number,
+		};
+	}
+
+	/**
+	 * Return an action to update a WeCare contact's infos
+	 * @param  {Number} options.id     The contact's ID
+	 * @param  {String} options.name   The contact's name
+	 * @param  {String} options.number The contact's phone number
+	 * @return {Object}                
+	 */
+	static updateContact({id, name, number}) {
+		return {
+			type: this.UPDATE_CONTACT,
 			id,
 			name,
 			number,
--- app/store/reducers/wecare.js
+++ app/store/reducers/wecare.js
@@ -1,7 +1,13 @@
 import WeCareAction from '../actions/wecare';
 
 const initialState = {
-	contacts: [],
+	contacts: [
+		{
+			id: 1,
+			name: 'Troy',
+			number: '+18171232018',
+		},
+	],
 };
 
 export default function wecare(state = initialState, action) {
@@ -13,6 +19,16 @@ export default function wecare(state = initialState, action) {
 					...state.contacts,
 					{name: action.name, number: action.number, id: action.id},
 				]
+			};
+
+		case WeCareAction.UPDATE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.map((contact) => {
+					return contact.id == action.id 
+						? { id: action.id, name: action.name, number: action.number }
+						: contact;
+				}),
 			};
 
 		case WeCareAction.DELETE_CONTACT:
