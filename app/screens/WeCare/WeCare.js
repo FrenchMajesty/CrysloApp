f6f0426befea7e8c83eac908fb5e63a30aa6fb92
Verdi.K <verdi@victron-tech.com>
2018-05-06 17:45:16 -0500

    Update WeCare screen to a functional component
    After connecting the contact list from the store I realized that as of now, this screen does not need to be state-aware so I updated it to a functional component.

--- app/screens/WeCare/WeCare.js
+++ app/screens/WeCare/WeCare.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import { connect } from 'react-redux';
 import Header from 'app/components/Header/';
@@ -8,25 +8,15 @@ import ContactList from 'app/components/WeCare/ContactList/';
 import styling from 'app/config/styling';
 import style from './style';
 
-class WeCare extends Component {
-
-	/**
-	 * Component constructor
-	 */
-	constructor(props) {
-		super(props);
-
-		this.openContactDetails = this.openContactDetails.bind(this);
-	}
+const WeCare = ({navigation, contacts, screenProps: {rootNavigation}}) => {
 
 	/**
 	 * Open the contact's details screen
 	 * @param {number} contactId Contact's ID
 	 * @return {Void} 
 	 */
-	openContactDetails(contactId) {
-		const {navigation, contacts: contactsList} = this.props;
-		const contact = contactsList.find(contact => contact.id == contactId);
+	const openContactDetails = (contactId) => {
+		const contact = contacts.find(contact => contact.id == contactId);
 
 		navigation.navigate('ContactDetails', {contact, mode: 'update'});
 	}
@@ -35,20 +25,15 @@ class WeCare extends Component {
 	 * Render the button to add additional emergency contacts
 	 * @return {ReactElement} 
 	 */
-	renderAddButton() {
-		const {navigation, contacts} = this.props;
-
-		const text = () => {
-			if(contacts.length >= 3) {
-				return 'You have reached the limit';
-			}
-
-			return `Add ${3-contacts.length} more`;
-		}
+	const renderAddButton = () => {
+		const text = contacts.length >= 3 ?
+						'You have reached the limit' 
+					:
+						`Add ${3-contacts.length} more`;
 
 		return (
 			<Button
-				text={text()}
+				text={text}
 				style={{width: '70%'}}
 				disabled={contacts.length >= 3}
 				onPress={() => navigation.navigate('ContactDetails', {mode: 'add'})}
@@ -56,45 +41,36 @@ class WeCare extends Component {
 		);
 	}
 
-	/**
-	 * Render the component
-	 * @return {ReactElement} Markup
-	 */
-	render() {
-		const {rootNavigation} = this.props.screenProps;
-		const {contacts} = this.props;
-
-		return (
-			<View style={[styling.grayScreenBackground]}>
-				<View style={{backgroundColor: '#fff', height: 140}}>
-					<Header
-						navigation={rootNavigation}
-						title="WeCare"
-						hint="These contacts are the people we are going to notify via text or call if your vitals go into your danger zone or if you press the panic button."
-						elevated={true}
-						style={{flex: 2, position: 'absolute'}}
-					/>
-				</View>
-				<ScrollView style={[styling.screenPadding, style.listContainer]}>
-					{contacts.length > 0 ?
-					<ContactList 
-						data={contacts} 
-						onItemPress={this.openContactDetails}
-					/>
-					:
-						<View style={{marginTop: 35, marginBottom: 35}}>
-							<CommonText weight="heavy" style={{fontSize: 16, textAlign: 'center'}}>
-								You have not added any emergency contacts to your account yet!
-							</CommonText>
-						</View>
-					}
-					<View style={[style.buttonStyle]}>
-						{this.renderAddButton()}
-					</View>
-				</ScrollView>
+	return (
+		<View style={[styling.grayScreenBackground]}>
+			<View style={{backgroundColor: '#fff', height: 140}}>
+				<Header
+					navigation={rootNavigation}
+					title="WeCare"
+					hint="These contacts are the people we are going to notify via text or call if your vitals go into your danger zone or if you press the panic button."
+					elevated={true}
+					style={{flex: 2, position: 'absolute'}}
+				/>
 			</View>
-		);
-	}
+			<ScrollView style={[styling.screenPadding, style.listContainer]}>
+				{contacts.length > 0 ?
+				<ContactList 
+					data={contacts} 
+					onItemPress={openContactDetails}
+				/>
+				:
+					<View style={{marginTop: 35, marginBottom: 35}}>
+						<CommonText weight="heavy" style={{fontSize: 16, textAlign: 'center'}}>
+							You have not added any emergency contacts to your account yet!
+						</CommonText>
+					</View>
+				}
+				<View style={[style.buttonStyle]}>
+					{renderAddButton()}
+				</View>
+			</ScrollView>
+		</View>
+	);
 };
 
 /**
