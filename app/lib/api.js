a8acd0566eb64452ee0e817eb4b0d10fb98c00d6
Verdi.K <verdi@victron-tech.com>
2018-05-08 19:37:19 -0500

    Add validateNumber and verifyCode method to API lib
    These two are needed So I can validate that a phone number is valid and un-used during the signup and forgotten password process.

--- app/lib/api.js
+++ app/lib/api.js
@@ -2,5 +2,13 @@ import { API_URL }  from 'app/config/globals';
 import axios from 'axios';
 
 export function validateEmail(email) {
-	return axios.post(`${API_URL}/validate/email`, {email})
+	return axios.post(`${API_URL}/validate/email`, {email});
+}
+
+export function validateNumber(number) {
+	return axios.post(`${API_URL}/validate/number`, {number});
+}
+
+export function verifyCode({number, code}) {
+	return axios.post(`${API_URL}/validate/verify`, {number, code});
 }
