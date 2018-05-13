32767c6c9c4a826247e8f802b77d86503ed97309
Verdi.K <verdi@victron-tech.com>
2018-05-12 19:55:31 -0500

    Add API method to load user profile
    Since the file is getting long I decided to do some formatting in the meantime.

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,5 +1,10 @@
-import { API_URL }  from 'app/config/globals';
 import axios from 'axios';
+import { API_URL }  from 'app/config/globals';
+import store from 'app/store/store';
+
+/**
+ * Verification and validation endpoints
+ */
 
 export function validateEmail(email) {
 	return axios.post(`${API_URL}/validate/email`, {email});
@@ -17,10 +22,29 @@ export function verifyCode({number, code}) {
 	return axios.post(`${API_URL}/validate/verify`, {number, code});
 }
 
+/**
+ * Auth endpoints
+ */
+
 export function signUp({email, password, number}) {	
 	return axios.post(`${API_URL}/users`, {number, password, email});
 }
 
 export function login({email, password}) {	
 	return axios.post(`${API_URL}/login`, {password, email});
+}
+
+/**
+ * Account actions endpoints
+ */
+
+const authHeaders = (token = '') => {
+	return {
+		headers: { Authorization: `Bearer ${token}` }
+	};
+};
+
+export function loadProfile(query = '') {	
+	const {token} = store.getState().profile.auth;
+	return axios.get(`${API_URL}/user${query}`, authHeaders(token));
 }
