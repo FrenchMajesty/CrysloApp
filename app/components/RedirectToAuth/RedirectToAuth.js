8159e274e60749bf0e7a1216a4690442f647409a
Verdi.K <verdi@victron-tech.com>
2018-05-13 17:44:04 -0500

    Clear the profile from store's state in RedirectToAuth component
    Whenever a user was logging out, the state wasn't being cleared. The profile info as well as the JWT token was still in the store's state so I added the dispatch of the resetState action in order to reset it to initial value.

--- app/components/RedirectToAuth/RedirectToAuth.js
+++ app/components/RedirectToAuth/RedirectToAuth.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { logout } from 'app/lib/api';
+import ProfileAction from 'app/store/actions/profile';
 
-const RedirectToAuth = ({screenProps: {authNav}}) => {
+const RedirectToAuth = ({resetState, screenProps: {authNav}}) => {
 
+	resetState();
 	authNav.navigate('Auth');
 
 	return <View />;
 };
 
-export default RedirectToAuth;
+/**
+ * Map the store's action dispatcher to the component's props
+ * @param  {Function} dispatch The dispatch function
+ * @return {Object}           
+ */
+const mapDispatchToProps = (dispatch) => ({
+	resetState: () => dispatch(ProfileAction.resetState()),
+});
+
+export default connect(null, mapDispatchToProps)(RedirectToAuth);
