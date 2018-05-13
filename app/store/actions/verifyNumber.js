9c12d0d5950fbddabfcadc5b90b01ab3637a55f6
Verdi.K <verdi@victron-tech.com>
2018-05-13 16:34:34 -0500

    Add `SET_USER_ID` action to verifyNumber reducer
    The user ID is needed when verifying the number for resetting a user's password.

--- app/store/actions/verifyNumber.js
+++ app/store/actions/verifyNumber.js
@@ -1,6 +1,7 @@
 export default class VerifyNumber {
 
 	static SET_PHONE_NUMBER = 'SET_PHONE_NUMBER';
+	static SET_USER_ID = 'SET_USER_ID';
 
 	/**
 	 * Return the action to update the user's phone number
@@ -11,6 +12,18 @@ export default class VerifyNumber {
 		return {
 			type: this.SET_PHONE_NUMBER,
 			number,
+		};
+	}
+
+	/**
+	 * Return the action to update the user's ID 
+	 * @param  {Number} id The user's ID
+	 * @return {Object}        
+	 */
+	static setUserId(id) {
+		return {
+			type: this.SET_USER_ID,
+			id,
 		};
 	}
 }
--- app/store/reducers/verifyNumber.js
+++ app/store/reducers/verifyNumber.js
@@ -2,12 +2,17 @@ import Actions from '../actions/verifyNumber';
 
 const initialState = {
 	number: '',
+	id: '',
 };
 
 export default function verifyNumber(state = initialState, action) {
 	switch(action.type) {
 		case Actions.SET_PHONE_NUMBER:
 			return {...state, number: action.number};
+
+		case Actions.SET_USER_ID:
+			return {...state, id: action.id};
+
 		default:
 			return state;
 	}
